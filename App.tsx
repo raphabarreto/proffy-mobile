bbe24d7c16db597f9ceb832db7f2fb1ba42eef4b
Raphael Barreto <tnt.raphael@gmail.com>
2020-08-06 16:32:24 -0300

    Finalizado layout e componentes - Dia 4

--- App.tsx
+++ App.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StatusBar } from 'react-native';
+import { AppLoading } from 'expo';
+
+import {
+  Archivo_400Regular,
+  Archivo_700Bold,
+  useFonts,
+} from '@expo-google-fonts/archivo';
+
+import {
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+} from '@expo-google-fonts/poppins';
+
+import AppStack from './src/routes/AppStack';
 
 const App: React.FC = () => {
-  return <Text>Texto</Text>;
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  return (
+    <>
+      <AppStack />
+      <StatusBar barStyle="light-content" backgroundColor="#8257E5" />
+    </>
+  );
 };
 
 export default App;
